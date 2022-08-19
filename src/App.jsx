32e84ff16d7fd1be05b346609ffb19c747111fe4
Daniel <danirvicente@gmail.com>
2022-08-19 20:10:53 +0200

    exercise 2.9 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react'
 
 const App = () => {
+
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: ' 040-123456' },
+    { name: 'Ada Lovelace', number: ' 39-44-5323523' },
+    { name: 'Dan Abramov', number: ' 12-43-234345' },
+    { name: 'Mary Poppendieck', number: ' 39-23-6423122' }
   ]) 
+
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filterPerson , setFilterPerson ] = useState('')
 
 
   const addPerson = (event) => {
@@ -14,6 +21,7 @@ const App = () => {
     }else{
       const newPerson = {
         name: newName,
+        number: ' ' + newNumber,
       }
       setPersons(persons.concat(newPerson));
       setNewName('')
@@ -23,18 +31,24 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>filter shown with <input value={filterPerson} onChange={event => setFilterPerson(event.target.value)} /></p>
+      <h3>add a new</h3>
       <form>
         <div>
-          name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
+        name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)}/>
         </div>
         <div>
           <button type="submit" onClick={addPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person => <li key={person.name}>{person.name}</li>)}
-      </ul>
+        {persons
+        .filter(person => person.name.toLowerCase().includes(filterPerson))
+        .map(person => <div key={person.number}>{person.name}{person.number}</div>)
+        }
     </div>
   )
 }
